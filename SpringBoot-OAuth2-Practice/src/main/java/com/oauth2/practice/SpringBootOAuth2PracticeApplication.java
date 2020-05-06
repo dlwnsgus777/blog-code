53e066ee5791bd806a3ff5337f6b04afed7ed459
Lee, JunHyun <dlwnsgus777@naver.com>
2020-05-06 15:34:22 +0900

    feat: 스프링부트에서 oauth2 로그인 하기 프로젝트 생성

---
+++ SpringBoot-OAuth2-Practice/src/main/java/com/oauth2/practice/SpringBootOAuth2PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.oauth2.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOAuth2PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOAuth2PracticeApplication.class, args);
+	}
+
+}
