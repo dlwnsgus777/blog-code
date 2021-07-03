3c49c679ba393915f8b877d0ffc0dbee74e73b57
JunHyun Lee <dlwnsgus777@naver.com>
2021-07-03 22:35:25 +0900

    feat: data-jdbc 연습프로젝트 생성
    feat: data-jdbc 연습프로젝트 생성

---
+++ data-jdbc-practice/src/test/java/com/practice/datajdbcpractice/DataJdbcPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.datajdbcpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJdbcPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
