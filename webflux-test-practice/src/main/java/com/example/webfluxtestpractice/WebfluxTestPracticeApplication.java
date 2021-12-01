4b44ccc6cf9d35f9c83821fbf0a8c89e49f489fe
Ted <dlwnsgus777@naver.com>
2021-12-01 14:23:02 +0900

    feat: 테스트 코드 연습 모듈 생성
    feat: 테스트 코드 연습 모듈 생성

---
+++ webflux-test-practice/src/main/java/com/example/webfluxtestpractice/WebfluxTestPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxtestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxTestPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxTestPracticeApplication.class, args);
+	}
+
+}
---
+++ webflux-test-practice/src/test/java/com/example/webfluxtestpractice/WebfluxTestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxtestpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxTestPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
