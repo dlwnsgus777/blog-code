347090a4a1f610dc8581ca95a933962fac45a0fc
Ted <dlwnsgus777@naver.com>
2022-01-04 08:28:05 +0900

    feat: reactor 연습 프로젝트 생성
    feat: reactor 연습 프로젝트 생성

--- webflux-test-practice/src/main/java/com/example/webfluxtestpractice/WebfluxTestPracticeApplication.java
+++ webflux-test-practice/src/main/java/com/example/webfluxtestpractice/WebfluxTestPracticeApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebfluxTestPracticeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebfluxTestPracticeApplication.class, args);
+		System.out.println("WebfluxTestPracticeApplication.main");
+//		SpringApplication.run(WebfluxTestPracticeApplication.class, args);
 	}
 
 }
