6772e9438af6e92282aec4aa426118efc4b007be
Ted <dlwnsgus777@naver.com>
2023-09-11 08:52:53 +0900

    feat: 팩토리 패턴 예제 버전 1작성
    feat: 팩토리 패턴 예제 버전 1작성

---
+++ example/src/main/java/com/design/factory/version1/BasicPizza.java
@@ -0,0 +1,25 @@
+package com.design.factory.version1;
+
+import com.design.factory.Pizza;
+
+public class BasicPizza implements Pizza {
+   @Override
+   public void prepare() {
+
+   }
+
+   @Override
+   public void bake() {
+
+   }
+
+   @Override
+   public void cut() {
+
+   }
+
+   @Override
+   public void box() {
+
+   }
+}
---
+++ example/src/main/java/com/design/factory/version1/PizzaStore.java
@@ -0,0 +1,25 @@
+package com.design.factory.version1;
+
+import com.design.factory.Pizza;
+
+public class PizzaStore {
+
+   public Pizza orderPizza(String type) {
+      Pizza pizza = null;
+
+      if (type.equals("cheese")) {
+         pizza = new CheesPizza();
+      } else if (type.equals("greek")) {
+         pizza = new GreekPizza();
+      } else if (type.equals("pepperoni")) {
+         pizza = new Pepperoni();
+      }
+
+      pizza.prepare();
+      pizza.bake();
+      pizza.cut();
+      pizza.box();
+      return pizza;
+   }
+
+}
