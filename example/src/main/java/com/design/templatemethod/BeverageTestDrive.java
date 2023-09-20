15529bcb7d419c1821cbd2c391b46734c37710b7
Ted <dlwnsgus777@naver.com>
2023-09-20 12:58:38 +0900

    Update BeverageTestDrive.java

--- example/src/main/java/com/design/templatemethod/BeverageTestDrive.java
+++ example/src/main/java/com/design/templatemethod/BeverageTestDrive.java
@@ -2,8 +2,10 @@ package com.design.templatemethod;
 
 class BeverageTestDrive {
     public static void main(String[] args) {
-        CoffeeWithHook coffeeHook = new CoffeeWithHook();
-        System.out.println("커피 준비 중");
-        coffeeHook.prepareRecipe();
+        CaffeineBeverage coffee = new Coffee();
+        coffee.prepareRecipe();
+//        CoffeeWithHook coffeeHook = new CoffeeWithHook();
+//        System.out.println("커피 준비 중");
+//        coffeeHook.prepareRecipe();
     }
 }
