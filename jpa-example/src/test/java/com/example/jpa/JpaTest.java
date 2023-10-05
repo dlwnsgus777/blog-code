bd8bfdf0d1df0078bbabf290aa71b1d35d317c69
JunHyun Lee <dlwnsgus777@naver.com>
2023-10-05 14:18:44 +0900

    feat: jpa 예제 코드 작성
    feat: jpa 예제 코드 작성

--- jpa-example/src/test/java/com/example/jpa/JpaTest.java
+++ jpa-example/src/test/java/com/example/jpa/JpaTest.java
@@ -17,7 +17,20 @@ public class JpaTest {
   @Test
   @Transactional
   void jpaTest() {
+
+    // 비영속 상태 - 객체를 생성한 상태
     Member member = new Member();
+    member.setName("test");
+
+    // 영속 상태 - 엔티티 매니저를 통해 영속성 컨텍스트에 저장한 상태
     em.persist(member);
+
+    // 준영속 상태 - 엔티티를 영속성 컨텍스트에서 분리한 상태
+    em.detach(member);
+    em.close();
+    em.clear();
+
+    // 삭제 - 객체를 삭제한 상태
+    em.remove(member);
   }
 }
