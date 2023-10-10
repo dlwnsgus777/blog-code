565d126959d8b5e7cadf14fa67e838b7a29a1781
JunHyun Lee <dlwnsgus777@naver.com>
2023-10-10 13:00:45 +0900

    feat: 예제 코드 작성
    feat: 예제 코드 작성

--- jpa-example/src/main/java/com/example/jpa/entity/Member.java
+++ jpa-example/src/main/java/com/example/jpa/entity/Member.java
@@ -1,9 +1,16 @@
 package com.example.jpa.entity;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "members")
+@Table(name = "members",
+uniqueConstraints = {
+        @UniqueConstraint(
+                name = "NAME_UNIQUE",
+                columnNames = {"name"}
+        )
+})
 public class Member {
   public Member() {
   }
@@ -12,8 +19,44 @@ public class Member {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column
+  @Column(name = "name", nullable = false, length = 10)
   private String name;
+
+  private Integer age;
+
+  @Enumerated(EnumType.STRING)
+  private RoleType roleType;
+
+  public enum RoleType {
+    ADMIN, USER
+  }
+
+
+  private LocalDateTime createdDate;
+  private LocalDateTime lastModifiedDate;
+
+  @Lob
+  private String description;
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public RoleType getRoleType() {
+    return roleType;
+  }
+
+  public LocalDateTime getCreatedDate() {
+    return createdDate;
+  }
+
+  public LocalDateTime getLastModifiedDate() {
+    return lastModifiedDate;
+  }
+
+  public String getDescription() {
+    return description;
+  }
 
   public Long getId() {
     return id;
@@ -25,5 +68,29 @@ public class Member {
 
   public void setName(final String name) {
     this.name = name;
+  }
+
+  public void setId(final Long id) {
+    this.id = id;
+  }
+
+  public void setAge(final Integer age) {
+    this.age = age;
+  }
+
+  public void setRoleType(final RoleType roleType) {
+    this.roleType = roleType;
+  }
+
+  public void setCreatedDate(final LocalDateTime createdDate) {
+    this.createdDate = createdDate;
+  }
+
+  public void setLastModifiedDate(final LocalDateTime lastModifiedDate) {
+    this.lastModifiedDate = lastModifiedDate;
+  }
+
+  public void setDescription(final String description) {
+    this.description = description;
   }
 }
