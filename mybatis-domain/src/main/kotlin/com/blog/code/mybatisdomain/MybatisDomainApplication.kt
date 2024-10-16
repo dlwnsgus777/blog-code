db6324b4cb1e4a49a802bd1fbe2f924f9c56518d
dlwnsgus777 <dlwnsgus777@naver.com>
2024-10-16 15:49:23 +0900

    feat: mybatis 도메인 분리 예제 모듈 추가

---
+++ mybatis-domain/src/main/kotlin/com/blog/code/mybatisdomain/MybatisDomainApplication.kt
@@ -0,0 +1,11 @@
+package com.blog.code.mybatisdomain
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MybatisDomainApplication
+
+fun main(args: Array<String>) {
+	runApplication<MybatisDomainApplication>(*args)
+}
