a349cead3e8299bf54d2ace3a8e3cf15fcb823d3
dlwnsgus777 <dlwnsgus777@naver.com>
2024-10-17 23:02:51 +0900

    feat: kotest 추가

---
+++ mybatis-domain/src/test/kotlin/com/blog/code/mybatisdomain/member/application/MemberServiceTest.kt
@@ -0,0 +1,13 @@
+package com.blog.code.mybatisdomain.member.application
+
+import io.kotest.core.spec.style.AnnotationSpec
+
+
+class MemberServiceTest : AnnotationSpec() {
+
+    @Test
+    fun `테스트해보자`() {
+        println("test")
+    }
+
+}
